7348a7ad7afdd027e315eccff163b1fbae658304
Your Name <you@example.com>
2024-03-14 13:45:37 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -169,6 +169,7 @@ Nodo *crearListaEnlazada(int arr[], int size) {
   for(int i = 0; i < size ; i++)
     {
     Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
+    nuevoNodo->numero = arr[i];
     if (actual == NULL) {
       primero = nuevoNodo;
       actual = nuevoNodo;
